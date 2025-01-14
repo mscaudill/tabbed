99c5b578221eb43768247a254b4cdde4d196e879
mscaudill <mscaudill@gmail.com>
2025-01-14 10:46:45 -0600

    [dev, refactor, lint, typing]: tabs for rows and columns complete with examples in docstrings

--- tabs.py
+++ tabs.py
@@ -1,32 +1,71 @@
+"""A module for creating & tracking tabs along rows & columns of tabular data.
+
+Tabs are callables that return a boolean indicating if a single column or row
+should be returned or skipped. These callables are defined in the
+Tabbed.utils.filters module and are automatically constructed for clients from
+kwargs passed to the 'tab' method of a Columns or Rows instance.
 """
 
-"""
-
-import operator as op
 from collections import abc
-from functools import partial, reduce
+from functools import partial
+from functools import reduce
+import operator as op
 import re
+from typing import Callable, Dict, List, Optional, Sequence
 
-from typing import Callable, Dict, List, Sequence
-from datetime import datetime
-
-from tabbed.utils import tabtypes, filters
+from tabbed.utils import celltyping
+from tabbed.utils import filters
+from tabbed.utils.celltyping import CellType
+from tabbed.utils.celltyping import Comparable
 from tabbed.utils.mixins import ReprMixin
 
 
-class Columns(abc.Callable, ReprMixin):
-    """A callable that sets & reads column names from tabular data.
+class Columns(ReprMixin):
+    """A Callable that sets & reads specific columns from tabular data.
 
     Tabular data is defined as a list of dicts of named data read by Python's
-    csv.DictRreader class. This object tracks and reads specific column names,
-    called tabs, from this data.
+    csv.DictRreader class. This object tracks and reads specific columns of the
+    data by applying a callable, the tabs attribute, to rows of data.
 
     Attributes:
         header:
             A dict of all possible column names, a subset of which can be set
-            using the 'set' method for reading.
+            using the 'tab' method for reading.
         tabs:
-            The subset of column names of header this Callable is set to read.
+            A callable the determines which columns of tabular data will be
+            returned by this callable. A tab may be a Sequence of strings, ints
+            or re.Pattern instance for selective filtering of the header column
+            names.
+
+    Example:
+        >>> # make some tabular data
+        >>> header = ['group', 'count', 'color', 'temp']
+        >>> group = ['a', 'c', 'b', 'b', 'c', 'a', 'c', 'b', 'c', 'a', 'a', 'c']
+        >>> count = [22,   2,   13,  15,  4,   19,  4,   21,  5,   24,  18,  1]
+        >>> color = 'r g b b r r r g g  b b g'.split()
+        >>> temp =  [33.5, 40.1, 29, 22, 20, 19.34, 50.0, 11.09, 11, 12, 11, 13]
+        >>> # simulate rows of data arriving from a csv.DictReader instance
+        >>> data = [
+        ... dict(zip(header, items)) for items in zip(group, count, color, temp)
+        ... ]
+        >>> # make a columns instance
+        >>> columns = Columns(header)
+        >>> # set tabs on columns to read just group and color
+        >>> columns.tab(['group', 'color'])
+        >>> columns(data)
+        ... #doctest: +NORMALIZE_WHITESPACE
+        [{'group': 'a', 'color': 'r'},
+         {'group': 'c', 'color': 'g'},
+         {'group': 'b', 'color': 'b'},
+         {'group': 'b', 'color': 'b'},
+         {'group': 'c', 'color': 'r'},
+         {'group': 'a', 'color': 'r'},
+         {'group': 'c', 'color': 'r'},
+         {'group': 'b', 'color': 'g'},
+         {'group': 'c', 'color': 'g'},
+         {'group': 'a', 'color': 'b'},
+         {'group': 'a', 'color': 'b'},
+         {'group': 'c', 'color': 'g'}]
     """
 
     def __init__(self, header: List[str]) -> None:
@@ -34,124 +73,73 @@ class Columns(abc.Callable, ReprMixin):
 
         Args:
             header:
-                A list of all possible column names in a tabular dataset.
+                A dict of all possible column names in a tabular dataset.
 
         Returns:
             None
         """
 
-        self.header = header
-        self._header = dict(zip(header, header))
-        self._tabs = header
-
+        self.header = dict(zip(header, header))
+        self._tabs: Optional[partial[bool]] = None
 
     @property
-    def tabs(self) -> List[str]:
-        """Return the column names to be read."""
+    def tabs(self) -> partial[bool] | None:
+        """Return the tab function that will filter the tabular data columns."""
 
         return self._tabs
 
-
     @tabs.deleter
     def tabs(self) -> None:
-        """On deletion, reset tabs to all names in header."""
+        """On deletion, reset tabs to None."""
 
-        self._tabs = self.header
+        self._tabs = None
 
-
-    def _membership(self, others: List[str]) -> List[str]:
-        """Sets tabs to read using a membership filter.
-
-        Args:
-            other:
-                A list of name tabs. The intersection of this list with the
-                header names will be returned.
-
-        Returns:
-            An others lengthed list of string column names.
-
-        Raises:
-            An IndexError is issued if any string in other is not in header.
-        """
-
-        if not set(self.header).issuperset(others):
-            invalid = set(other).difference(header)
-            raise IndexError(f'[{invalid}] are not a valid column name(s)')
-
-        func = partial(filters.membership, row=self._header, other=others)
-        result = [name for name in self.header if func(name=name)]
-
-        return result
-
-
-    def _index(self, indices: List[int]) -> List[str]:
-        """Sets tabs to read by indexing this instance's header.
+    def tab(self, elements: Sequence[str | int] | re.Pattern) -> None:
+        """Set the tab function that will filter the tabular data columns.
 
         Args:
-            indices:
-                Indices of the header names to store to this instance.
-
-        Returns:
-            A indices lengthed list of string column names.
-
-        Raises:
-            An IndexError is issued if any index exceeds headers length.
-        """
-
-        return [self.header[idx] for idx in indices]
-
-
-    def _regex(self, pattern: re.Pattern) -> List[str]:
-        """Sets tabs to read by re pattern matching with this instance's
-        header.
-
-        Args:
-            pattern:
-                A compiled regular expression pattern, an re.Pattern instance.
-                Each name in header will be searched for this pattern and stored
-                if a match is found.
-
-        Returns:
-            A list of strings, header names in which pattern is found. If no
-            matches are found an empty list is returned.
-        """
-
-        func = partial(filters.search, row=self._header, other=pattern)
-        result = [name for name in self.header if func(name=name)]
-
-        return result
-
-
-    def tab(self, tabs: Sequence[str|int] | re.Pattern) -> None:
-        """Set the tabs this instance will read from tabular data.
-
-        Args:
-            tabs:
-                A sequence of strings, integers or a single re pattern denoting
-                the column names to read. If a sequence of ints, header names
-                with matching indices will be stored for reading, if a sequence
-                of strings, header names that string match will be stored for
-                reading and if a compiled re pattern, all header names
-                containing pattern will be stored for reading.
+            elements:
+                A sequence of strings, integers or a single re pattern
+                determining the column names to read. If a sequence of ints,
+                header names with matching indices will be stored for reading,
+                if a sequence of strings, header names that string match will be
+                stored for reading and if a compiled re pattern, all header
+                names containing pattern will be stored for reading.
 
         Returns:
             None
+
+        Raises:
+            A TypeError is issued if elements is not a Sequence or re.Pattern
+            An IndexError is issued of elements is a sequence of strings with at
+            least one element not in the header.
         """
 
-        if isinstance(tabs, re.Pattern):
-            result = self._regex(tabs)
+        if not isinstance(elements, (Sequence, re.Pattern)):
+            msg = 'Tab elements must be a Sequence or re.Pattern type'
+            raise TypeError(msg)
 
-        elif isinstance(tabs[0], int):
-            result = self._index(tabs)
+        if isinstance(elements, re.Pattern):
+            # re search of header column names
+            func = filters.search
+            self._tabs = partial(func, row=self.header, other=elements)
+
+        elif isinstance(elements[0], int):
+            # index of header column names
+            func = filters.index
+            self._tabs = partial(func, other=elements)
 
         else:
-            result = self._membership(tabs)
+            # membership of header column names
+            if not set(self.header).issuperset(elements):
+                invalid = set(elements).difference(self.header)
+                raise IndexError(f'[{invalid}] are not a valid column name(s)')
 
-        self._tabs = result
+            func = filters.membership
+            self._tabs = partial(func, row=self.header, other=elements)
 
-
-    def __call__(self, data: List[Dict[str, tabtypes.CellType]]):
-        """Reads the data for each name stored in this instance.
+    def __call__(self, data: List[Dict[str, celltyping.CellType]]):
+        """Reads the columns of data that have been tabbed by this instance.
 
         Args:
             data:
@@ -163,42 +151,104 @@ class Columns(abc.Callable, ReprMixin):
             tab names of this instance.
         """
 
+        if not self.tabs:
+            return data
+
+        # narrow type for mypy
+        assert isinstance(self._tabs, partial)
+
+        # get the tabbed names by calling tabs
+        # pylint misses that tabs is a partial with a func member
+        # pylint: disable-next = no-member
+        if self.tabs.func.__name__ == 'index':
+            h = list(self.header.keys())
+            names = [h[idx] for idx in range(len(h)) if self._tabs(idx=idx)]
+        else:
+            names = [name for name in self.header if self._tabs(name=name)]
+
+        # extract only whose name is in tabbed names
         result = []
         for row in data:
-            result.append({name: row[name] for name in self.tabs})
+            result.append({name: row[name] for name in names})
 
         return result
 
 
-class Rows(abc.Callable, ReprMixin):
-    """A callable that sets & reads rows from tabular data.
+class Rows(ReprMixin):
+    """A Callable that sets & reads rows from tabular data.
 
     Tabular data is defined as a list of dicts of named data read by Python's
-    csv.DictRreader class. This object tracks partial functions each of which
-    accepts a dictionary row & returns a boolean indicating if the row should be part
-    of the return. The partial functions can perform membership test, comparison
-    test, re pattern match tests, equality test or run a custom callable test.
+    csv.DictRreader class. This object tracks and reads specific rows of the
+    data by applying a list of callables, the tabs attribute, to each row of
+    data. These callables are defined in tabbed.utils.filters and are
+    automatically set using the kwargs passed to this instance's 'tab' method.
 
     Attributes:
         tabs:
             A list of partial functions that accept only a dict representing
             a single row of tabular data. These filtering functions return
             a boolean indicating if the row should be included in the return.
- """
+
+    Example:
+        >>> from datetime import datetime, timedelta
+        >>> # make tabular data
+        >>> header = ['group', 'count', 'color', 'temp', 'time']
+        >>> group = ['a', 'c', 'b', 'b', 'c', 'a', 'c', 'b', 'c', 'a', 'a', 'c']
+        >>> count = [22,   2,   13,  15,  4,   19,  4,   21,  5,   24,  18,  1]
+        >>> color = 'r g b b r r r g g  b b g'.split()
+        >>> temp =  [33.5, 40.1, 29, 22, 20, 19.34, 50.0, 11.09, 11, 12, 11, 13]
+        >>> time = [
+        ... datetime(2025, 1, 14, 10, 12, 13) + timedelta(seconds=10*i)
+        ... for i in range(12)
+        ... ]
+        >>> # simulate rows of data arriving from a csv.DictReader instance
+        >>> data = [
+        ... dict(zip(header, items)) for items in
+        ... zip(group, count, color, temp, time)
+        ... ]
+        >>> # build a rows instance
+        >>> rows = Rows(header)
+        >>> # tab rows with group a or c, time < 10:14:00, and count in (0, 10)
+        >>> rows.tab(
+        ... group=re.compile(r'a|c'), time='<01-14-2025 10:14:00', count=range(10)
+        ... )
+        >>> # apply the tabs by calling the rows instance on data
+        >>> row_results = rows(data)
+        >>> row_results
+        ... #doctest: +NORMALIZE_WHITESPACE
+        [{'group': 'c',
+          'count': 2,
+          'color': 'g',
+          'temp': 40.1,
+          'time': datetime.datetime(2025, 1, 14, 10, 12, 23)},
+         {'group': 'c',
+          'count': 4,
+          'color': 'r',
+          'temp': 20,
+          'time': datetime.datetime(2025, 1, 14, 10, 12, 53)},
+         {'group': 'c',
+          'count': 4,
+          'color': 'r',
+          'temp': 50.0,
+          'time': datetime.datetime(2025, 1, 14, 10, 13, 13)},
+         {'group': 'c',
+          'count': 5,
+          'color': 'g',
+          'temp': 11,
+          'time': datetime.datetime(2025, 1, 14, 10, 13, 33)}]
+    """
 
     def __init__(self, header: List[str]):
         """Initialize this Rows instance with a header of column names."""
 
         self.header = header
-        self._tabs = []
-
+        self._tabs: List = []
 
     @property
-    def tabs(self) -> List[Callable]:
+    def tabs(self) -> List[partial[bool]]:
         """Returns a list of filtering functions determining row inclusion."""
 
         return self._tabs
-
 
     @tabs.deleter
     def tabs(self) -> None:
@@ -206,13 +256,13 @@ class Rows(abc.Callable, ReprMixin):
 
         self._tabs = []
 
-
     def tab(
         self,
         **kwargs: Dict[
-                    str,
-                    CellType|Sequence[CellType]|re.Pattern|Callable[dict, bool]],
-        ) -> None:
+            str,
+            CellType | Sequence[CellType] | re.Pattern | Callable,
+        ],
+    ) -> None:
         """Sets a tab, a boolean filter that will be applied to each row of
         tabular data.
 
@@ -222,8 +272,8 @@ class Rows(abc.Callable, ReprMixin):
                 - A CellType such as a str, int, float, complex, or datetime.
                   Values of this kind trigger an equality comparison of row[name]
                   with value.
-                - A string containing up to 2 rich comparison operators sepearated
-                  by a single logical and/or operator (eg.  '>10 and <100'.
+                - A string containing up to 2 rich comparison operators separated
+                  by a single logical and/or operator (eg.  '>10 and <100').
                   Values of this kind trigger comparison with row[name].
                 - A Sequence of CellType (e.g. ['a' ,'b']). This value triggers
                   a membership test of row[name] in value.
@@ -248,45 +298,45 @@ class Rows(abc.Callable, ReprMixin):
 
         for name, value in kwargs.items():
 
-            # comparison
-            if tabtypes.is_comparison(value):
+            # comparison is a subset of equality when value type is str
+            if celltyping.is_comparison(value):
                 func = partial(filters.comparison, name=name, other=value)
                 self._tabs.append(func)
 
             # equality
-            elif isinstance(value, tabtypes.CellType):
+            elif isinstance(value, celltyping.CellType):
                 func = partial(filters.equality, name=name, other=value)
                 self._tabs.append(func)
 
             # regex
-            elif tabtypes.is_regex(value):
+            elif celltyping.is_regex(value):
                 func = partial(filters.search, name=name, other=value)
                 self._tabs.append(func)
 
             # membership
-            elif tabtypes.is_sequence(value):
+            elif celltyping.is_sequence(value):
                 func = partial(filters.membership, name=name, other=value)
                 self._tabs.append(func)
 
-            # indicator callable
-            elif isinstance(value, Callable):
-               # other will be required to be a func of a single variable
-               func = partial(filters.indicator(name=name, other=other))
-               self._tabs.append(func)
+            # indicator callable -mypy has problems with callable types
+            elif isinstance(value, abc.Callable): # type: ignore[arg-type]
+                # other will be required to be a func of a single variable
+                func = partial(filters.indicator, name=name, other=value)
+                self._tabs.append(func)
 
             else:
-                msg = ("Conversion of value into a comparison, equality, regex,
-                        membership or indicator test failed for column '{name}'")
+                msg = (
+                    "Conversion of value into a comparison, equality, regex,"
+                    f"membership or indicator test failed for column '{name}'"
+                )
                 raise TypeError(msg)
-
 
     def __call__(
         self,
-        data,
+        data: List[Dict[str, CellType]],
         logical: Callable[[Comparable, Comparable], bool] = op.and_,
-        ) -> List[Dict[str, CellType]]:
-        """Applies all of the row filter tabs to each dictionary row in data
-        and reduces the result to a single boolean with logical.
+    ) -> List[Dict[str, CellType]]:
+        """Applies all of the row filter tabs to each dictionary row in data.
 
         Args:
             data:
@@ -296,7 +346,7 @@ class Rows(abc.Callable, ReprMixin):
                 a boolean. Defaults to Python's builtin operator and_.
         """
 
-        if not tabs:
+        if not self.tabs:
             return data
 
         results = []
@@ -307,39 +357,8 @@ class Rows(abc.Callable, ReprMixin):
         return results
 
 
-
 if __name__ == '__main__':
 
-    table = [['Genotype', 'marbles', 'color', 'stype'],
-             ['WT', 10, 'k', 1],
-             ['WT', 11, 'g', 1],
-             ['wt', 8, 'b', 1.2],
-             ['mut', 0, 'g', 1.0],
-             ['MUT', 2, 'g', 3],
-             ['MUT', 1, 'g', 3],
-             ['MUT', 4, 'g', 3],
-            ]
+    import doctest
 
-    header = table[0]
-    data = [dict(zip(table[0], ls)) for ls in table[1:]]
-
-    """
-    columns = Columns(table[0])
-    columns.tab(tabs=[0, 1,3])
-    print(columns.tabs)
-
-    pattern = re.compile(r'stype|mar|col')
-    columns.tab(pattern)
-    print(columns.tabs)
-
-    columns.tab(['Genotype', 'marbles', 'color'])
-    print(columns.tabs)
-
-    filtered = columns(data)
-    """
-
-    rows = Rows()
-    rows.tab(Genotype=['WT', 'MUT'], marbles='>1 and <=10',
-            color=re.compile(r'g|b'), stype=3)
-    results = rows(data)
-
+    doctest.testmod()
