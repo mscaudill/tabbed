d07e072fd4ac67b07fa701cd550c2a7c07a77d5e
mscaudill <mscaudill@gmail.com>
2025-01-29 13:15:55 -0600

    [dev]: wip on read method

--- reader.py
+++ reader.py
@@ -8,20 +8,17 @@ from itertools import islice
 from typing import List
 
 from tabbed import tabs
-from tabbed.sniffer import Sniffer
-from tabbed.utils.celltyping import convert
-from tabbed.utils.containers import Header, Meta
+from tabbed.sniffing import Header, MetaData, Sniffer
+from tabbed.utils.queues import ListFIFO
+from tabbed.utils.celltyping import CellType, convert
 
 # Planning
 #
-# 1. __init__ should take an open file instance to allow for readers to be
-#    initialized with and without context management
 # 2. reader should have a 'peek' or show method to show a sample of what will be
 #    read in a nice format
 # 3. reader should have an index method which will add a client named index to
 #    the rows prior to filters to allow for row index filtering
 # 4. reader will need to do auto type conversion using celltyping.convert
-# 5. reader should store the sniffed metadata and header
 # 6. reader will embedd a Rows and Columns instance for tab setting
 # 7. reader's read method should support chunk interation of rows that yield
 #    consistent sizes after filtering FIFO
@@ -29,34 +26,55 @@ from tabbed.utils.containers import Header, Meta
 #    with no imports
 # 9. Reader should have a sample method to show lines and indices to aid setting
 #    specific numbers like count etc. Could call the method 'lines'
-#
-# Questions:
-# 1. should we sniff file for header and metadata before constructing this
-#    reader or try to sniff during reader initialization?
-#    Pros of before:
-#    a. The most versatile as it will make no assumptions about the sample used
-#       to determine the dialect, the metadata and the header
-#    Cons of before:
-#    a. Users the would need to know how to use sniffers prior to reading
-#    SOLN: we will sniff during reader initialization
-#
 
 
 class Reader:
-    """ """
- 
-    def __init__(
-        self,
-        infile,
-        dialect = None,
-        **kwargs,
-        ) -> None:
-        """ """
+    """A reader of delimited text files supporting iterative reading of specific
+    rows and columns from files that possibly contain metadata and header
+    components.
+
+    Most CSV files do not adhere to the RFC-4180 CSV standards. In particular,
+    many delimited files contain a metadata section in addition to a header line
+    naming the columns of the data. Python's file sniffer can detect dialects
+    but it's heuristic rules for detecting the header are limited and exclude
+    the possibility of metadata. This reader uses an embedded sniffer to detect
+    header and metadata sections. Additionally, all delimited text readers to
+    date are unable to filter the data by value during reading. This reader
+    can filter both rows and columns of the data using equality, membership,
+    comparisons, regular expression matching and callables to select rows during
+    reading. These filters are stored to objects called Tabs that can be
+    dynamically changed from this Reader instance.
+
+    Attributes:
+        infile:
+            An I/O stream instance returned by open.
+        sniffer:
+            A tabbed Sniffer instance fo inferring the dialect and structure of
+            infile.
+        header:
+            A Header named tuple instance from sniffer. This value is
+            updated on any change to this Reader's sniffer.
+        columns:
+            A callable that determines the columns of the data to return during
+            read. This callable is set by creating a tab via the tab method
+            allowing columns to be selected by regex pattern, membership or index.
+        rows:
+            A sequence of callables that determines the rows of data to return
+            during read. These callables can be set by creating a tab via the
+            tab method allowing rows to be filtered by comparison, equality,
+            membership, regex or callable.
+
+    Example:
+        >>
+    """
+
+    def __init__(self, infile, **kwargs) -> None:
+        """Initialize Reader with infile & any valid sniffer arguments."""
 
         self.infile = infile
         self.sniffer = Sniffer(self.infile, **kwargs)
+        # set protected attributes accessible by property access only
         self._header = self.sniffer.header
-        # intercept sniff attr setting to update this reader's header
         self._set_sniff()
         self._columns = None
         self._rows = None
@@ -65,31 +83,35 @@ class Reader:
         """On update of a Sniffer attribute, update this readers header.
 
         Monkey patches Sniffer's __setattr__ method in order to update this
-        Reader's header whenever our embedded sniffer instance changes.
+        Reader's header whenever the embedded sniffer instance changes. This
+        protected method should not be externally called.
         """
 
         def on_change(sniffer, name, value):
             """A replacement for Sniffer's __setattr__."""
 
             super(Sniffer, sniffer).__setattr__(name, value)
-            self._header = sniffer.header
+            self.header = sniffer.header
 
         Sniffer.__setattr__ = on_change
 
     @property
     def header(self) -> Header | None:
-        """Returns the header from this reader's infile if it exist.
+        """Returns this Reader's header instance.
+
+        This header may come from the sniffer or a previously set header.
 
         Returns:
-            A namedtuple of header names & a file line number if existant.
+            An instance of the Header dataclass (see sniffing module).
         """
 
         item = self._header
+        # if header is a sniffer method call it
         return item() if isinstance(item, abc.Callable) else item
 
     @header.setter
     def header(self, value: int | List[str] | str) -> None:
-        """Set the header of this reader.
+        """Sets this Reader's header.
 
         Args:
             value:
@@ -117,9 +139,6 @@ class Reader:
         num_columns = len(self.sniffer.rows()[-1])
         if isinstance(value, int):
 
-            # TODO you'll need to sanitize each header here so pull sanitation
-            # out of sniffer to a utils func to be recalled 
-            # new sniffer to sniff single row at value
             sniffer = Sniffer(self.infile, start=value, amount=1)
             self._header = Header(sniffer.rows()[0], value)
 
@@ -171,8 +190,29 @@ class Reader:
 
         self._rows = None
 
-    def tab(self, columns=None, **rows):
-        """ """
+    def tab(
+        self,
+        columns: Optional[List[str|int] | re.Pattern] = None,
+        **rows: dict[
+            str,
+            CellType | Sequence[CellType] | re.Pattern | Callable],
+        ):
+        """Sets the rows or columns that will be read by this Reader during a
+        read method call.
+
+        Args:
+            columns:
+                A list of column names, column indices or regular expression
+                pattern to match column names. These columns will be the ones
+                returned. If None, all columns will be returned.
+            **rows:
+                A mapping of column names and one of; CellType, list of
+                Celltype, a compiled re pattern, a rich comparison or
+                a callable.
+
+        Returns:
+            None
+        """
 
         if columns:
             ctabs = tabs.Columns(self.header.names)
@@ -184,9 +224,61 @@ class Reader:
             rtabs.tab(**rows)
             self._rows = rtabs
 
-
-    def read(self, start=None, skips=None, chunksize=None, **fmtparams):
+    def read(
+        self,
+        start=None,
+        skips=None,
+        chunksize=None,
+        index_name='index',
+        **fmtparams,
+        ):
         """ """
+
+        if start is None:
+            start = self.header.line + 1 if self.header.line else 0
+
+        skips = [] if not skips else skips
+        chunksize = len(self) if not chunksize else chunksize
+        # advance to the data section
+        [next(self.infile) for _ in range(start))
+        dictreader = csv.DictReader(
+                    infile,
+                    self.header,
+                    restkey=None,
+                    restval='',
+                    dialect = self.sniffer.dialect, **fmtparams)
+
+        # TODO could update with custom converters if desired
+        converters = dict(zip(self.header, self.sniffer.types))
+        fifo = ListFIFO()
+        for line, dic in enumerate(dictreader, start):
+            if line in skips:
+                continue
+
+            unnamed = dic.pop(None)
+            converted = {name: converters[name](dic[name]) for name in dic}
+            converted.ins({index_name: line})
+            self._header.names.append(index_name) # this may be a problem frozen
+            filtered = self.rows(converted)
+            filtered = self.columns(filtered)
+        
+            if filtered:
+                # TODO FIFO will need to change since we place dict into it not
+                # list
+                fifo.put(filtered)
+
+
+
+
+
+
+    def _read(self, start=None, skips=None, chunksize=None, **fmtparams):
+        """ """
+
+
+        # To avoid dealing with infile yourself due to seperators at line
+        # endings you could just let DictReader do it and ignore the restkey
+        # remainder
 
         if start is None:
             start = self.header.index+1 if self.header.index else 0
@@ -194,6 +286,7 @@ class Reader:
         skips = [] if not skips else skips
 
         [next(self.infile) for _ in range(start)]
+
 
         sep = fmtparams.get('delimiter', self.sniffer.dialect.delimiter)
         results = []
@@ -231,7 +324,10 @@ class Reader:
         return results
 
 
+    def preview(self):
+        """ """
 
+        pass
 
     def close(self):
         """ """
