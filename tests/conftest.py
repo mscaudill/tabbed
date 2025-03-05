16fd5952c8ee00cc7f13a43551614249ba78c454
Brad Sheppard <bradsheppard@brads-mbp-2.dyndns.rice.edu>
2025-03-04 22:53:33 -0600

    Added some fixtures for creating tables, and added new shared_helper.py helper functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 import random
 import string
-from typing import Callable, Generator, TypeVar
 import re
+import itertools
 
-import helpers
+import shared_helper
 
 # Fixture for returning a random number generator
 @pytest.fixture
@@ -13,12 +13,13 @@ def rn_generator():
 
     return random.Random(1)
 
+
 # Numeric-related Fixtures
 @pytest.fixture
 def random_integers(rn_generator, request):
     """This returns a list of n random integers. n is passed in through indirect parameterization"""
 
-    return [helpers.make_random_integer(rn_generator) for _ in range(request.param)]
+    return [shared_helper.make_random_integer(rn_generator) for _ in range(request.param)]
     
 
 
@@ -26,34 +27,45 @@ def random_integers(rn_generator, request):
 def random_floats(rn_generator, request):
     """This returns a list of n random floats. n is passed in through indirect parameterization"""
 
-    return [helpers.make_random_float(rn_generator) for _ in range(request.param)]
+    return [shared_helper.make_random_float(rn_generator) for _ in range(request.param)]
+
+
 
 @pytest.fixture
 def random_complex_numbers(rn_generator, request):
     """This returns a list of n random complex numbers. n is passed in through indirect parameterization"""
-    
-    return [helpers.make_random_complex_num(rn_generator) for _ in range(request.param)]
+
+    return [shared_helper.make_random_complex_num(rn_generator) for _ in range(request.param)]
+
+
 
 @pytest.fixture
 def random_scientific_notation_floats(rn_generator, request):
     """Returns a list of n scientific notation floats. n is passed in through indirect parameterization"""
 
-    return [helpers.make_random_sci_not_float(rn_generator) for _ in range(request.param)]
+    return [shared_helper.make_random_sci_not_float(rn_generator) for _ in range(request.param)]
+
+
 
 @pytest.fixture
 def random_numerics(rn_generator, request):
     """Returns a list of n numerics. n is passed in through indirect parameterization"""
 
-    return [helpers.make_random_numeric(rn_generator) for _ in range(request.param)]
+    return [shared_helper.make_random_numeric(rn_generator) for _ in range(request.param)]
+
+
 
 @pytest.fixture
 def random_non_numerics(rn_generator, request):
-    """Returns a generator of random non-numeric strings"""
+    """Returns a list of n non-numerics. n is specified is an indirect parameter"""
 
-    return [helpers.make_random_non_numeric(rn_generator) for _ in range(request.param)]
+    return [shared_helper.make_random_non_numeric(rn_generator) for _ in range(request.param)]
+
 
 @pytest.fixture
 def random_strings(rn_generator, request):
-    """Returns a generator of random strings"""
+    """Returns a list of n random strings. n is specified as an indirect parameter"""
 
-    return [helpers.make_random_string(rn_generator) for _ in range(request.param)]
+    return [shared_helper.make_random_string(rn_generator) for _ in range(request.param)]
+
+
