f71b7e3d44f38f5424c21590f0eaf39bcf43165e
mscaudill <mscaudill@gmail.com>
2025-03-31 10:51:32 -0500

    [tests]: randomness now under full control of rng fixture by parameterization and new header and metadata fixtures done

--- tests/test_sniffer.py
+++ tests/test_sniffer.py
@@ -11,7 +11,9 @@ from itertools import batched
 from tabbed.sniffing import Sniffer
 
 
-@pytest.fixture(params=[0])
+NUM_TEST = 3
+
+@pytest.fixture(params=range(NUM_TEST))
 def rng(request):
     """Returns a random number generator."""
 
@@ -19,9 +21,9 @@ def rng(request):
     return random
 
 
-@pytest.fixture(params=",;|\t")
-def delimiter(request):
-    """Returns tabbed compatible delimiters."""
+@pytest.fixture(params=[',', ';', '|', r'\t'])
+def delimiters(request):
+    """Returns a subset of tabbed compatible delimiters."""
 
     return request.param
 
@@ -33,7 +35,10 @@ def valid_chars():
     Invalid characters are a delimiters and quote chars
     """
 
-    chars = list(string.ascii_letters + string.digits + string.punctuation)
+    chars = list(string.ascii_letters +
+                 string.digits +
+                 string.punctuation +
+                 ' ')
     invalid = set([',', ';', '|', '\t', '\'', '"'])
 
     return [char for char in chars if char not in invalid]
@@ -41,12 +46,8 @@ def valid_chars():
 
 @pytest.fixture
 def string_table(rng, valid_chars):
-    """A fixture that returns a function for building random tables of valid
-    string characters."""
+    """Returns a function for building random tables of valid string chars."""
 
-    # FIXME when I call this fixture over and over in table I want to produce
-    # new random tables that are reproducible across table calls. I think
-    # turning rng into  a function returning fixture is the way to go
     def make_table(rows, cols):
         """Returns a rows x cols table of strings of random lengths."""
 
@@ -61,8 +62,7 @@ def string_table(rng, valid_chars):
 
 @pytest.fixture
 def integer_table(rng):
-    """A fixture that returns a function for building random tables of stringed
-    integer values."""
+    """Returns a function for building random tables of stringed integers."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of integers between -1000 and 1000."""
@@ -77,8 +77,7 @@ def integer_table(rng):
 
 @pytest.fixture
 def float_table(rng):
-    """A fixture that returns a function for building random tables of stringed
-    float values."""
+    """Returns a function for building random tables of stringed floats."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of stringed floats in [-1000, 1000]."""
@@ -93,8 +92,7 @@ def float_table(rng):
 
 @pytest.fixture
 def complex_table(rng):
-    """A fixture that returns a function for building random tables of stringed
-    complex values."""
+    """Returns a func. for building random tables of stringed complex values."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of stringed complex values with real and
@@ -112,8 +110,8 @@ def complex_table(rng):
 
 @pytest.fixture
 def enotation_table(rng):
-    """A fixture that returns a function for building random tables of stringed
-    scientific notation float values."""
+    """Returns a function for building random tables of stringed scientific
+    notation values."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of E notation floats in [-1000, 1000]."""
@@ -128,8 +126,7 @@ def enotation_table(rng):
 
 @pytest.fixture
 def time_table(rng, time_formats):
-    """A fixture that returns a function for building random tables of stringed
-    times."""
+    """Returns a function for building random tables of stringed times."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of times."""
@@ -153,8 +150,7 @@ def time_table(rng, time_formats):
 
 @pytest.fixture
 def date_table(rng, date_formats):
-    """A fixture that returns a function for building random tables of stringed
-    dates."""
+    """Returns a function for building random tables of stringed dates."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of dates."""
@@ -174,8 +170,7 @@ def date_table(rng, date_formats):
 
 @pytest.fixture
 def datetime_table(time_table, date_table):
-    """A fixture that returns a function for building random tables of stringed
-    datetime instances."""
+    """Returns a function for building random tables of stringed datetimes."""
 
     def make_table(rows, cols):
         """Returns a rows x cols table of datetime strings."""
@@ -192,14 +187,12 @@ def datetime_table(time_table, date_table):
     return make_table
 
 
-@pytest.fixture(params=[2])
-def shape(request):
-    """A fixture that returns a reproducible random shape tuple for a table."""
+@pytest.fixture
+def shape(rng):
+    """Returns a reproducible random shape tuple for a table."""
 
-    # to generate different shapes if rng seed is not None use fresh random
-    random.seed(request.param)
-    row_count = random.randint(1, 200)
-    col_count = random.randint(1, 20)
+    row_count = rng.randint(1, 200)
+    col_count = rng.randint(1, 20)
 
     return row_count, col_count
 
@@ -216,8 +209,11 @@ def table(
         date_table,
         datetime_table,
 ):
-    """Returns random tables of random shapes with random tabbed supported
-    data types."""
+    """Returns a randomly shaped table for each rng of the rng fixture.
+
+    This table is constructed from single type tables for all types supported by
+    Tabbed.
+    """
 
     args = locals()
     rng = args.pop('rng')
@@ -234,54 +230,219 @@ def table(
     # transpose back to cols by rows and return
     return list(zip(*chosen))
 
-# FIXME
-# I don't yet full have the randomization to my satisfaction. I want to generate
-# sequences of random but reproducible tables. This code with the current rng
-# does not accomplish this
+
+# TODO test what happens for invalid tables, specifically
+# 1. tables with invalid types (expect string returns) e.g. stringed tuples
+# 2. ragged tables with rows of different numbers of columns
+
+
+@pytest.fixture
+def header(rng, valid_chars):
+    """Returns a function for building a random valid header."""
+
+    def make_header(cols):
+        """Returns a list of valid header names."""
+
+        return [''.join(rng.choices(valid_chars, k=15)) for _ in range(cols)]
+
+    return make_header
+
+
+@pytest.fixture
+def repeating_header(rng, valid_chars):
+    """Returns a function for building headers with repeating names."""
+
+    def make_header(cols):
+        """Returns a list of valid header names with repeats of length cols."""
+
+        names = [''.join(rng.choices(valid_chars, k=15)) for _ in range(cols)]
+        indices = rng.choices(range(len(names)), k=min(len(names) // 2, 1))
+        for idx in indices:
+            names[idx] = names[0]
+
+        return names
+
+    return make_header
+
+
+@pytest.fixture
+def numeric_header(rng):
+    """Returns a function for building an invalid numeric header."""
+
+    def make_header(cols):
+        """Returns a list of stringed numeric names."""
+
+        return [str(rng.randint(0, 100)) for _ in range(cols)]
+
+    return make_header
+
+
+@pytest.fixture
+def date_header(rng):
+    """Returns invalid headers composed of stringed date names."""
+
+    def make_header(cols):
+        """Returns a list of stringed date names."""
+
+        start = datetime.datetime(2023, 9, 13)
+        days = [rng.randint(0, 500) for _ in range(cols)]
+        deltas = [datetime.timedelta(days=d) for d in days]
+
+        return [str((start + delta).date()) for delta in deltas]
+
+    return make_header
+
+
+@pytest.fixture
+def time_header(rng):
+    """Returns invalid headers composed of stringed time names."""
+
+    def make_header(cols):
+        """Returns a list of stringed time names."""
+
+        start = datetime.datetime(1, 1, 1, 10, 32, 15, 63200)
+        secs = [rng.randint(0, 10000) for _ in range(cols)]
+        deltas = [datetime.timedelta(seconds=sec) for sec in secs]
+
+        return [str((start + delta).time()) for delta in deltas]
+
+    return make_header
+
+
+@pytest.fixture
+def metadata(rng, valid_chars, delimiters):
+    """Return valid metadata strings for each rng and delimiter."""
+
+    num_lines = rng.randint(1, 30)
+    cell_counts = [rng.randint(1, 8) for _ in range(num_lines)]
+
+    lines = []
+    for cell_count in cell_counts:
+        line = [''.join(rng.choices(valid_chars, k=16)) for _ in range(cell_count)]
+        lines.append(delimiters.join(line))
+
+    return '\n'.join(lines)
+
+
+@pytest.fixture
+def empty_metadata():
+    """Returns a metadata with only new line characters."""
+
+    return '\n'.join([''] * 10)
+
+
+@pytest.fixture
+def skipping_metadata(rng, valid_chars):
+    """Returns a metadata with a blank line."""
+
+    rows = list(batched(rng.choices(valid_chars, k=100), 5))
+    lines = [';'.join(row) for row in rows]
+    lines[3] = ''
+
+    return '\n'.join(lines)
 
 
 
+
+
+
+
+
+
+
+"""
+def test_validchars(valid_chars):
+
+    print(valid_chars)
+
+    assert True
+"""
+
+
+"""
 def test_stringtable(string_table):
 
     print(string_table(4, 5))
 
     assert True
+"""
 
+"""
 def test_inttabel(integer_table):
 
     print(integer_table(4, 5))
 
     assert True
+"""
 
+"""
+def test_datetable(date_table):
+
+    print(date_table(3,6))
+
+    assert True
+"""
 
 
 """
 def test_table(table):
-    """ """
 
-    print('shape =', len(table), len(table[0])) 
+    print('shape =', len(table), len(table[0]))
     for row in table:
         print(row)
 
     assert True
 """
 
-
 """
-def test_shapes(shape):
-    """ """
+def test_header(header):
 
-    print(shape)
-
-    assert True
-
-def test_rng(rng):
-
-    a = [rng.randint(0,100) for _ in range(10)]
-    b = [rng.randint(0,100) for _ in range(10)]
-
-    print(a)
-    print(b)
+    print(header(5))
 
     assert True
 """
+
+"""
+def test_repeat_header(repeating_header):
+
+    print(repeating_header(4))
+
+    assert True
+"""
+
+"""
+def test_numericheader(numeric_header):
+
+    print(numeric_header)
+    assert True
+"""
+
+"""
+def test_dateheader(date_header):
+
+    print(date_header(6))
+    assert True
+"""
+
+"""
+def test_timeheader(time_header):
+
+    print(time_header(10))
+
+    assert True
+"""
+
+"""
+def test_metadata(metadata):
+
+    print(metadata)
+
+    assert True
+"""
+
+def test_skippingmetadata(skipping_metadata):
+
+    print(skipping_metadata)
+
+    assert True
+
