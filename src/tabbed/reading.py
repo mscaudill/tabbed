8bdea3ac50bd24ae1baea1c2896af1e633e7c6e1
mscaudill <mscaudill@gmail.com>
2025-05-28 12:28:17 -0500

    [refactor]: upto read method of new reader that will now allow tabulator to account for changes in the header, this was not in the old reader.

--- src/tabbed/reading.py
+++ src/tabbed/reading.py
@@ -2,53 +2,78 @@
 
 """
 from __future__ import annotations
+from types import SimpleNamespace
 from typing import Dict, IO, List, Optional
+import warnings
 
 from tabbed.sniffing import Header, Sniffer
+from tabbed.tabbing import Tabulator
 from tabbed.utils.mixins import ReprMixin
+from tabbed.utils import parsing
+from tabbed import tabbing
 
 
-class _HeaderDescriptor:
-    """ """
+class Reader(ReprMixin):
+    """
 
-    def __init__(self, storage_name: str = '_header'):
-        """ """
+    """
 
-        self.storage_name = storage_name
+    def __init__(self, infile:IO[str], **sniffing_kwargs) -> None:
+        """Initialize this Reader."""
 
-    def _from_integer(self, instance: Reader, value: int):
-        """Creates a Header instance from an integer index value."""
+        self.infile = infile
+        self._sniffer = Sniffer(infile, **sniffing_kwargs)
+        self._header = self._sniffer.header()
+        self.tabulator = Tabulator(self.header, columns=None, tabs=None)
+        self.errors = SimpleNamespace(casting=[], ragged=[])
 
-        sniff = Sniffer(instance.infile, start=value, amount=1)
-        string = sniff.sample[0]
+    @property
+    def sniffer(self) -> Sniffer:
+        """Returns this Reader's sniffer.
 
-        return Header(value, sniff.rows[0], string)
+        If this Reader's header has been determined by sniffing the infile, then
+        accessing the sniffer will remeasure the header and reset the Tabulator.
+        For example, if the start attribute of the sniffer is changed and the
+        header was previously determined by the sniffer, then the header will
+        be remeasured and the Tabulator reset.
+        """
 
-    def _from_list(self, instance: Reader, value: List[str]):
-        """Creates a Header instance from a list of string names."""
+        if self._header.line is not None:
+            print('Resniffing Header and resetting Tabulator')
+            self._header = self._sniffer.header()
+            self.tabulator = Tabulator(self.header, columns=None, tabs=None)
 
-        expected = len(instance.sniffer.rows[-1])
-        if len(value) != expected:
-            msg = (
-                    f'Length of provided header names = {len(value)} does '
-                    f'not match the number of sniffed columns = {expected}'
-                )
-            raise ValueError(msg)
+        return self._sniffer
 
-        return Header(None, value, None)
+    @property
+    def header(self) -> Header:
+        """Fetches this Reader's current header."""
 
-    def _from_kwargs(self, instance: Reader, value: Dict[str, int]):
-        """Creates a Header instance by Sniffer's header method."""
+        return self._header
 
-        return instance.sniffer.header(**value)
+    @header.setter
+    def header(self, value: int | List[str] | Dict[str, int]) -> None:
+        """Sets this Reader's header and resets the Tabulator.
 
-    def __get__(self, instance, owner):
-        """Returns the header from the Reader instance."""
+        Args:
+            value:
+                An infile line number, list of string names, or dict of keyword
+                arguments for sniffer's header method. If value is type int, the
+                header will be set to the split string values of the value row
+                of infile. If value is type List, the header will be set to the
+                string names in value. If value is type dict, the header will be
+                resniffed by sniffer's header method using value keyword args.
+                Valid keyword arguments are: 'poll', and 'exclude'. Please type
+                help(reader.sniffer.header) for more argument details.
 
-        return getattr(instance, self.storage_name)
+        Returns:
+            None
 
-    def __set__(self, instance, value):
-        """ """
+        Raises:
+            A ValueError is issued if value is int or List type and the length
+            of the proposed header names does not match the length of the last
+            sample row in the sniffer.
+        """
 
         if not isinstance(value, (int, list, dict)):
             msg = (
@@ -58,56 +83,202 @@ class _HeaderDescriptor:
                     )
             raise ValueError(msg)
 
+        # get the expected length of the header from the last sample row.
+        expected = len(self._sniffer.rows[-1])
+
         if isinstance(value, int):
-            header = self._from_integer(instance, value)
+            sniff = Sniffer(self.infile, start=value, amount=1)
+            if len(sniff.rows[0]) != expected:
+                    msg = (
+                        f'Length of row at index = {value} does not match'
+                        f'length of last sample row = {expected}'
+                        )
+                    raise ValueError(msg)
+            result = Header(value, sniff.rows[0], sniff.sample)
+
         if isinstance(value, list):
-            header = self._from_list(instance, value)
+            if len(value) != expected:
+                msg = (
+                    f'Length of provided header names = {len(value)} does '
+                    f'not match length of last sample row = {expected}'
+                )
+                raise ValueError(msg)
+            result = Header(None, value, None)
+
         if isinstance(value, dict):
-            header = self._from_kwargs(instance, value)
+            result = self._sniffer.header(**value)
 
-        setattr(instance, self.storage_name, header)
+        # set header
+        self._header = result
+        # determine if reader has previously set tabulator and warn
+        previous  = self.tabulator
+        tblr = Tabulator(self.header, tabs=None, columns=None)
+        if tblr.columns != previous.columns or tblr.tabs != previous.tabs:
+            msg = ("Previously set tabs have been reset. Please call 'tab' "
+                   "method again before reading.")
+            print(msg)
+        self.tabulator = tblr
 
+    def tab(
+        self,
+        columns: Optional[List[str | int] | re.Pattern] = None,
+        **tabs: dict[
+            str, CellType | Sequence[CellType] | re.Pattern | Callable
+        ],
+    ) -> None:
+        """Set the Tabulator instance that will filter infiles rows & columns.
 
+        A tabulator is a container of tab instances that when called on a row,
+        sequentially applies each tab to that row. Additionally after applying
+        the row tabs it filters the result by columns. Implementation details
+        may be found in the tabbed.tabs module.
 
-class Reader(ReprMixin):
-    """
+        Args:
+            columns:
+                Names of the columns in each row to return during reading. These
+                may be provided as a list of string names, a list of column
+                indices or a compiled regular expression pattern to match
+                against header names. If None, all the columns in the header
+                will be read during a read call.
+            tabs:
+                name = value keyword argument pairs where name is a valid header
+                column name and value may be of type string, int, float,
+                complex, time, date, datetime, regular expression or callable.
+                If a string type with rich comparison(s) is provided,
+                a comparison tab is constructed. If a string, int, float,
+                complex, time, date  or datetime is provided, an equality tab is
+                constructed. If a sequence is provided, a membership tab is
+                constructed. If a compiled re patterni, a Regex tab is
+                constructed. See class docs for example.
 
-    """
-
-    header = _HeaderDescriptor()
-
-    def __init__(self, infile:IO[str], **sniffing_kwargs):
-        """ """
-
-        self.infile = infile
-        self._sniffer = Sniffer(infile, **sniffing_kwargs)
-        self._header = self._sniffer.header()
-        self._tabs = []
-        self.errors = None
-
-
-    @property
-    def sniffer(self):
-        """Intercept any change to the sniffer and update the header if needed.
-
-        This applies to sniffer attr access or set so its overkill but it avoids
-        monkey patching
+        Returns:
+            None
         """
 
-        if self._header.line is not None:
-            print('updating header')
-            self._header = self._sniffer.header()
-        return self._sniffer
+        self.tabulator = tabbing.Tabulator.from_keywords(
+            self.header, columns, **tabs
+        )
+
+    def types(self, poll: int) -> Dict[str, parsing.CellType]:
+        """Polls the last poll rows of the sniffer's sample for types.
+
+        Args:
+            poll:
+                The number of last sample rows to poll for column types.
+
+        Returns:
+            A mapping of header names and inferred column types.
+        """
+
+        typings, consistent = self._sniffer.types(poll)
+        if not consistent:
+            msg = ('Reader detected inconsistent column types.'
+                   'Using most common type for each column.')
+            warnings.warn(msg)
+
+        return dict(zip(self.header.names, typings))
+
+    def _datastart(self) -> int:
+        """Returns the first line number of the data section.
+
+        Returns:
+            An integer line number.
+        """
+
+        start = 0
+        line = self.header.line
+        if line is not None:
+            # locate by header line
+            start = line + 1
+        else:
+            # locate by last line of metadata
+            start = self._sniffer.metadata(line).lines[1] + 1
+
+        return start
+
+    def _log_ragged(self, line, row, raise_error):
+        """Error logs rows whose length is unexpected.
+
+        When python's csv DictReader encounters a row with more cells than
+        header columns, it stores the additional cells to a list under the None
+        key.  When the csv DictReader encounters a row that with fewer cells
+        than header columns it inserts None values into the missing cells. This
+        function detects rows with None keys or None values and logs the row
+        number to the error log.
+
+        Args:
+            line:
+                The line number of the row being tested.
+            row:
+                A row dictionary of header names and casted values.
+            raise_error:
+                A boolean indicating if ragged should raise an error and stop
+                the reading of the file if a ragged row is encountered.
+
+        Returns:
+            The row with None restkey popped
+        """
+
+        remainder = row.pop(None, None)
+        none_vals = None in row.values()
+
+        if remainder is not None or none_vals:
+            msg = f'Unexpected line length on row {line}'
+            if raise_error:
+                raise csv.Error(msg)
+            self.errors.ragged.append(msg)
+
+        return row
+
+    def _log_miscast(self, line, row, expected):
+        """Error logs rows that have at least one type mismatch with expected.
+
+        Casting errors do not raise exceptions but are logged since they can be
+        handled after reading.
+
+        Args:
+            line:
+                The line number of the row being tested.
+            row:
+                A row dictionary of header names and casted values.
+
+        Returns:
+            The row.
+        """
+
+        types = {name: type(val) for name, val in row.items()}
+        miscast = {name for name in types if types[name] != row[name]}
+        if miscast:
+            msg = f"Casting error on line = {line}, column = '{miscast}'"
+            self.errors.casting.extend(msg)
+
+        return row
+
+    # TODO should read return either iterator or list or should we build
+    # a function to turn iterator into single list
+    def read(
+        self,
+        start: Optional[int] = None,
+        skips: Optional[Sequence[int]] = None,
+        indices: Optional[Sequence] = None,
+        chunksize: int = int(2e5),
+        skip_blanks: bool = True,
+        castings: Optional[Dict[str, Callable[[str], CellType]]] = None,
+        raise_ragged: bool = False,
+    ) -> Iterator[List[Dict[str, CellType]]]:
+        """ """
+
+        start = self._datastart() if not start else start
+        skips = [] if not skips else skips
+        indices
+
+
+
 
 
 if __name__ == '__main__':
 
-    fp = '/home/matt/python/nri/tabbed/__data__/mouse_annotations.txt'
+    fp = '/home/matt/python/nri/tabbed/__data__/fly_sample.txt'
 
     infile = open(fp, 'r')
-    reader = Reader(infile)
-
-    # header changing
-    #
-    # does changing sniffer change this instances sniffer
-
+    reader = Reader(infile) 
