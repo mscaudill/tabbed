b93b518fed53fa62d6fa857494b194b68cf05338
mscaudill <mscaudill@gmail.com>
2025-07-02 13:13:53 -0500

    [build]: make all modules available at package level

--- src/tabbed/__init__.py
+++ src/tabbed/__init__.py
@@ -1,3 +1,10 @@
 """Tabbed's version"""
 
 __version__ = "1.0.0"
+
+from . import reading
+from . import sniffing
+from . import tabbing
+from . import utils
+
+__all__ = ['reading', 'sniffing', 'tabbing', 'utils']
